53b5a93cd76dbee6484690efa1aa92ee43279c94
s28539 <s28539@pjwstk.edu.pl>
2024-03-12 15:07:24 +0100

    Modyfikacja 3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
+Console.WriteLine("Modyfikacja 3");
 Console.WriteLine("Hello, World!");
