cc52751760b055bda585ff7f629eccbea9a14748
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-03-19 18:47:04 +0100

    Change variable name on main

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,14 +17,14 @@ static double GetAverage(int[] numbers)
 
 static int GetMaxValue(int[] numbers)
 {
-    int max = numbers[0];
+    int max2 = numbers[0];
     for (int i = 0; i < numbers.Length; i++)
     {
-        if (max < numbers[i])
+        if (max2 < numbers[i])
         {
-            max = numbers[i];
+            max2 = numbers[i];
         }
     }
 
-    return max;
+    return max2;
 }
